56febd3fe878bfb0f0aba5d41a3c68864cd32714
wugimy <67146088+wugimy@users.noreply.github.com>
2024-01-17 00:20:26 +0800

    Add files via upload

---
+++ run.py
@@ -0,0 +1,186 @@
+from flask import Flask, render_template, redirect, url_for, request
+import sqlite3
+import pandas as pd # 引用套件並縮寫為 pd
+import json
+import os
+import openpyxl
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import pymysql
+
+# 資料庫參數設定
+db_settings = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "root",
+    "password": "gimy0710",
+    "db": "my_db",
+    "charset": "utf8"
+}
+
+def df_transform(df):
+    data = []
+    for row in df.values:
+        a = list(row[0:2])
+        for i in range(2,len(row)):
+            #a = [row[0],row[1],df.columns[i],row[i]]
+            b = a + [df.columns[i]]
+            b.append(row[i])
+            data.append(b)
+    df = pd.DataFrame(data,columns=['STAGE','ITEM','PERIOD','VAL'])
+    return df
+def get_df_from_mysql(SQL):
+    cn = pymysql.connect(**db_settings)
+    df = pd.read_sql(SQL, cn)
+    cn.close()
+    return df
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    cn = sqlite3.connect('my_db.db')
+    SQL = "select * from expense"
+    SQL = "select * from books"
+    df = pd.read_sql(SQL, cn)
+    cn.close()
+    jf = df.to_json(orient="records")
+    html = '<a href="/static/index.html">index</a>'
+    html += ' | <a href="/create_table">create table</a>'
+    html += ' | <a href="/insert">資料插入</a>'
+    html += ' | <a href="/web_scraper">爬蟲</a>'
+    html += '<h3>' + jf
+    html += '</h3>'
+    return html
+
+@app.route("/mysql_query", methods=["POST", "GET"])
+def mysql_query():
+    SQL = "select * from cost_course where MFG_DAY='2023/11/04' and STAGE='A'"
+    if request.method == "POST":
+        SQL = request.form["sql"]
+    df = get_df_from_mysql(SQL)
+    jf = df.to_json(orient="records", date_format="iso")
+    return jf
+    
+@app.route("/json_query")
+def json_query():
+    cn = sqlite3.connect('my_db.db')
+    SQL = "select * from expense"
+    SQL = "select * from books"
+    df = pd.read_sql(SQL, cn)
+    cn.close()
+    jf = df.to_json(orient="records")
+    return jf
+    
+    
+@app.route("/create_table")
+def create_table():
+    cn = sqlite3.connect('my_db.db')
+    SQL = "create table IF NOT EXISTS expense (PERIOD date, CLASS2 char(10), EXPENSE int)"
+    SQL = "create table IF NOT EXISTS books (id char(50) primary key,book_name char(50),result char(10),grade int, pass_datetime datetime)"
+    cn.execute(SQL)
+    cn.close()
+    return '<a href="/">expense 表格建立完成</a>'
+
+@app.route("/insert")
+def insert():
+    cn = sqlite3.connect('my_db.db')
+    SQL = "insert into expense (PERIOD,CLASS2,EXPENSE) values ('2023-08-01','維護費用',10000)"
+    cn.execute(SQL)
+    cn.commit()
+    cn.close()
+    return '<a href="/">資料插入一筆</a>'
+
+@app.route("/add_book")
+def add_book():
+    cn = sqlite3.connect('my_db.db')
+    SQL = "insert OR IGNORE into books (id) values ('test')"
+    cn.execute(SQL)
+    cn.commit()
+    cn.close()
+    return '<a href="/static/index.html">back</a>'
+
+@app.route("/del_book")
+def del_book():
+    cn = sqlite3.connect('my_db.db')
+    SQL = "delete from books where  id='test'"
+    cn.execute(SQL)
+    cn.commit()
+    cn.close()
+    return '<a href="/static/index.html">back</a>'
+
+@app.route('/excel/<sheet_name>')
+def excel(sheet_name):
+    path = r'D:\doc\test.xlsx'
+    wb = openpyxl.load_workbook(path)
+    #sheet_name = wb.sheetnames[0]
+    #s = wb[sheet_name]
+    s = wb[wb.sheetnames[0]]
+    s['A10'].value = sheet_name
+    wb.save(path)
+    wb.close()
+    os.system(path)
+    #return f'<a href="/excel/bbb">Hello</a> {sheet_name}'
+    return '<a href="/excel/aaa">aaa</a> <a href="/excel/bbb">bbb</a> '
+
+@app.route("/web_scraper")
+def web_scraper():
+    driver = webdriver.Edge()
+    url = "https://read.tc.edu.tw/member_login_new.php"
+    driver.get(url) # 更改網址以前往不同網頁
+    a = driver.find_element('id', 'student')
+    a.click()
+    a = driver.find_element(By.XPATH, "//input[@type='radio' and @value='s_uname']")
+    a.click()
+    a = driver.find_element(By.NAME, "stu_username")
+    a.send_keys('ltestyc111030604')
+    a = driver.find_element(By.NAME, "password")
+    a.send_keys('71565')
+    a = driver.find_element(By.NAME, "submit_item")
+    a.click()
+    url = "https://read.tc.edu.tw/stu_account_number_1.php"
+    driver.get(url) # 更改網址以前往不同網頁
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.close() # 關閉瀏覽器視窗
+    
+    cn = sqlite3.connect('my_db.db')
+    tr = soup.find_all('tr')
+    for i in tr:
+        td = i.find_all('td')
+        if len(td) > 5:
+            print(td[1].text,td[2].text,td[3].text,td[4].text,td[5].text)
+            SQL = "INSERT OR IGNORE INTO books (id) VALUES ('" + td[1].text + "')"
+            cn.execute(SQL)
+            pass_datetime = td[5].text.split('~')[0]
+            SQL = "update books set book_name='" + td[2].text + "',result='" + td[3].text + "',grade='" + td[4].text + "',pass_datetime='" + pass_datetime + "' where id='" + td[1].text + "'"
+            cn.execute(SQL)
+    cn.commit()
+    cn.close()
+    return '<a href="/">爬蟲執行完成</a>'
+    
+@app.route("/hello")
+def hello():
+    return "Hello World! This is Hello Page "
+    
+if __name__ == "__main__":
+    app.run()
+    
+    
+'''
+from flask import Flask
+import webbrowser
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    a = "Hello Flask!"
+    return a
+    
+
+if __name__ == "__main__":
+    app.run()
+    url='http://127.0.0.1:5000/'
+    webbrowser.get('windows-default').open_new(url)
+'''
---
+++ static/highcharts_fun.js
@@ -0,0 +1,298 @@
+Highcharts.theme = {
+    colors: ['#005087','#64dcf0','#ffe664','#b491d7','#87dc64','#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
+        '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+	credits:{
+		enabled: false
+	},
+	legend:{ enabled:true },
+    title: {
+        text: ''
+    },
+
+    xAxis: {
+        title: {
+            text: ''
+        },
+        labels: {
+            format: ''
+        }	
+    },
+    yAxis: [
+		{
+        title: {
+            text: ''
+        }
+		},{
+        title: {
+            text: ''
+        }
+		,opposite: true
+		}
+	],
+    plotOptions: {
+        line: {
+			lineWidth: 4,
+            dataLabels: {
+                enabled: true,
+				style: {
+                fontSize: '16px',
+                fontFamily: 'Verdana, sans-serif'
+				}
+            },
+            enableMouseTracking: false
+        },
+		column: {
+            dataLabels: {
+                enabled: true,
+				inside: true,
+				verticalAlign: 'middle',
+				style: {
+                fontSize: '16px',
+                fontFamily: 'Verdana, sans-serif'
+				}
+            }
+		},
+		bar: {
+            dataLabels: {
+                enabled: true,
+				style: {
+                fontSize: '16px',
+                fontFamily: 'Verdana, sans-serif'
+				}
+            }
+		}
+    }
+}
+// Apply the theme
+Highcharts.setOptions(Highcharts.theme);
+
+
+//JSON輸出表格
+function json_to_table(json_data) {
+    t = '<table class="table-bordered table-striped small"><tr class="bg-info text-light">';
+    for(var key in json_data[0]){
+        t += "<td>" + key + "</td>";
+    }
+    t += "</tr>";
+    for (var i = 0; i < json_data.length; i++) {
+        t += "<tr>";
+        for(var key in json_data[i]){
+            t += ("<td>" + json_data[i][key] + "</td>");
+        }
+        t += ("</tr>");
+    }
+    t += ("</table>");
+    return t
+}
+function json_to_heatmap(json){
+	categories = Object.keys(json[0]);
+	firstElement = categories.shift();
+	max = 0;
+	for(var i=0;i<json.length;i++){
+		for(j in categories){
+			if(json[i][categories[j]] > max){
+				max = json[i][categories[j]];
+			}
+		}
+	}
+	t = '<table class="table-bordered text-center w-100">';
+	t += '<tr class="bg-info text-light">';
+	for(key in json[0]){
+		t += '<th>' + key + '</th>';
+	}
+	t += '</tr>';
+	for(var i=0;i<json.length;i++){
+		t += '<tr>';
+		t += '<td>' + json[i][firstElement] + '</td>';
+		for(j in categories){
+			key = categories[j];
+			r = json[i][key]/max;
+			color = 'black';
+			if(r > 0.6){
+				color = 'white';
+			}
+			t += '<td style="background: rgba(76, 175, 80, ' + r + ');color:' + color + '">' + json[i][key] + '</td>';
+		}
+		t += '</tr>';
+	}
+	t += '</table>';
+	return t;
+}
+function show_card_header(title,result){
+	if(result > 0){
+		document.write('<div class="card-header bg-danger text-light"><h6>' + title + '</h6></div>');
+	}
+	else{
+		document.write('<div class="card-header bg-success text-light"><h6>' + title + '</h6></div>');
+	}
+}
+function show_card(title,result){
+	document.write('<div class="col-md-6"><div class="card shadow m-2">');
+	if(result > 0){
+		document.write('<div class="card-header bg-danger text-light"><h6>' + title + '</h6></div>');
+	}
+	else{
+		document.write('<div class="card-header bg-success text-light"><h6>' + title + '</h6></div>');
+	}
+	document.write('<div class="card-body">' + result + '</div>');
+	document.write('</div></div>');
+}
+function json_col(json_data,col){
+	arr = [];
+	for(var i=0;i<json_data.length;i++){
+		arr.push(json_data[i][col])
+	}
+	return arr;
+}
+function json_row(json,gb,categories){
+	series = [];
+	for(var i=0;i<json.length;i++){
+		data = [];
+		for(col_name of categories.values()){
+			data.push(json[i][col_name]);
+		}
+		series.push({name:json[i][gb],data:data});
+	}
+	return series;
+}
+function json_xy(json_data,x,y){
+	xy = [];
+	for(var i=0;i<json_data.length;i++){
+		xy.push([json_data[i][x],json_data[i][y]])
+	}
+	return xy;
+}
+function num_float(num,f){
+	return Math.round(num*(10**f))/(10**f)
+}
+function num_format(num,unit,f){
+	str = '';
+	op = unit.substr(0,1);
+	if(op=='+'){
+		if(num > 0){
+			str += '+';
+		}
+		else{
+			str += '-';
+		}
+		num = Math.abs(num);
+		unit = unit.substr(1,1);
+	}
+	if(unit=='K'){
+		num = num / 1000;
+	}
+	else if(unit=='萬'){
+		num = num / 10000;
+	}
+	else if(unit=='億'){
+		num = num / 100000000;
+	}
+	else if(unit=='M'){
+		num = num / 1000000;
+	}
+	else if(unit=='%'){
+		num = num * 100;
+	}
+	if(unit=='$'){
+		str += '$';
+		unit = '';
+	}
+	str += Math.round(num*(10**f))/(10**f);
+	str += unit;
+	return str;
+}
+
+function show_chart(cid,categories,series,options){
+	if('col_len' in options){
+		document.write('<div class="col-md-' + options['col_len'] + '">');
+	}
+	else{
+		document.write('<div>');
+	}
+	document.write('<div class="card shadow">');
+	if('color' in options){
+		document.write('<div class="card-header" style="background:' + options['color'] + ';">');
+	}
+	else{
+		document.write('<div class="card-header">');
+	}
+	document.write('<h5>' + cid + '</h5></div>');
+	document.write('<div id="' + cid + '"></div>');
+	document.write('</div>');
+	
+	document.write('</div>');
+	
+	
+	yAxis = [];
+	if('y_max' in options){
+		yAxis.push({title:{text:''},max:options['y_max']});
+	}
+	else{
+		yAxis.push({title:{text:''}});
+	}
+	if('y_max' in options){
+		yAxis.push({title:{text:''},min:options['y_min'],opposite: true});
+	}
+	else{
+		yAxis.push({title:{text:''},opposite: true});
+	}
+	
+	Highcharts.chart(cid, {
+		chart: {
+			type: 'line'
+		},
+		xAxis:{
+			categories: categories
+		},
+		yAxis:yAxis,
+		series: series
+	});
+}
+function arr_max(arr){
+	max = 0;
+	for(var i=0;i<arr.length;i++){
+		if(arr[i] > max){
+			max = arr[i];
+		}
+	}
+	return max;
+}
+
+function show_chart_in_col(cid,categories,series,color,col_len){
+	document.write('<div class="col-md-' + col_len + '">');
+	document.write('<div class="card shadow">');
+	document.write('<div class="card-header" style="background:' + color + '"><h5 class="m-0">' + cid + '</h5></div>');
+	document.write('<div id="' + cid + '"></div>');
+	document.write('</div>');
+	document.write('</div>');
+	
+	Highcharts.chart(cid, {
+		chart: {
+			type: 'line'
+		},
+		xAxis:{
+			categories: categories
+		},
+		yAxis:[
+		{title:{text:''}},
+		{title:{text:''},opposite: true}
+		],
+		series: series
+	});
+}
+function to_acc(arr){
+	new_arr = [];
+	for(var i=0;i<arr.length;i++){
+		new_arr.push(arr[i]);
+	}
+	for(var i=1;i<arr.length;i++){
+		new_arr[i] += new_arr[i-1];
+	}
+	return new_arr;
+}
+function to_left(arr,len){
+	for(var i=0;i<arr.length;i++){
+		arr[i] = arr[i].substr(0,len);
+	}
+	return arr;
+}
